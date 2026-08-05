71918a2d5c47f8fdb2b33adbe2e79d6662677495
zain0333 <zain03337631969@gmail.com>
2026-08-05 02:26:09 -0700

    thirty-seven commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import App from "./App.tsx";
+import "./App.css";
 
-import "./index.css";
-
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
     <App />
-  </React.StrictMode>,
+  </StrictMode>,
 );
