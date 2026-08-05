e6e916211caabe941a846e17f7331c0ac880f36a
zain0333 <zain03337631969@gmail.com>
2026-08-05 00:24:34 -0700

    six commit

---
+++ src/data/product.ts
@@ -0,0 +1,35 @@
+import cricket from "../images/cricket.jpg";
+import football from "../images/football.jpg";
+import badminton from "../images/badminton.jpg";
+import gym from "../images/gym.jpg";
+
+export const products = [
+  {
+    id: 1,
+    name: "Cricket Bat",
+    category: "Cricket",
+    price: 5000,
+    image: cricket,
+  },
+  {
+    id: 2,
+    name: "Football",
+    category: "Football",
+    price: 2500,
+    image: football,
+  },
+  {
+    id: 3,
+    name: "Badminton Racket",
+    category: "Badminton",
+    price: 3500,
+    image: badminton,
+  },
+  {
+    id: 4,
+    name: "Dumbbells",
+    category: "Gym",
+    price: 7000,
+    image: gym,
+  },
+];
