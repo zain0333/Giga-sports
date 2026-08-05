045ac34c6580b93175b6e0d495be6840c68aa049
zain0333 <zain03337631969@gmail.com>
2026-08-05 01:13:51 -0700

    fiveteen commit

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,22 +1,58 @@
 import { Link } from "react-router-dom";
 import { FaShoppingCart } from "react-icons/fa";
+import { useCart } from "../context/CartContext";
 
 function Navbar() {
+  const { cart } = useCart();
+
   return (
-    <nav className="navbar navbar-dark bg-dark">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container">
         <Link className="navbar-brand fw-bold" to="/">
-          GIGA SPORTS SHOP
+          🏏 GIGA SPORTS SHOP
         </Link>
 
-        <div>
-          <Link className="btn btn-outline-light me-2" to="/">
-            Home
-          </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
 
-          <Link className="btn btn-warning" to="/cart">
-            <FaShoppingCart /> Cart
-          </Link>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <Link className="nav-link" to="/">
+                Home
+              </Link>
+            </li>
+
+            <li className="nav-item">
+              <Link className="nav-link" to="/products">
+                Products
+              </Link>
+            </li>
+
+            <li className="nav-item">
+              <Link className="nav-link" to="/about">
+                About
+              </Link>
+            </li>
+
+            <li className="nav-item">
+              <Link className="nav-link" to="/contact">
+                Contact
+              </Link>
+            </li>
+
+            <li className="nav-item">
+              <Link className="nav-link" to="/cart">
+                <FaShoppingCart /> Cart ({cart.length})
+              </Link>
+            </li>
+          </ul>
         </div>
       </div>
     </nav>
