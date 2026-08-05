87b085284f67bc73379e2fa4693b836c8c44cad4
zain0333 <zain03337631969@gmail.com>
2026-08-05 01:19:00 -0700

    seventeen commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
+
 import Home from "./pages/Home";
+import Cart from "./pages/Cart";
+
 import { CartProvider } from "./context/CartContext";
 
 function App() {
@@ -12,6 +15,8 @@ function App() {
 
         <Routes>
           <Route path="/" element={<Home />} />
+
+          <Route path="/cart" element={<Cart />} />
         </Routes>
       </BrowserRouter>
     </CartProvider>
