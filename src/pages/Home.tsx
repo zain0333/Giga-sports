2c5779b8dae594069e5d82f62af4a46457752b31
zain0333 <zain03337631969@gmail.com>
2026-08-05 00:17:34 -0700

    second commit

---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+function Home() {
+  return (
+    <div className="container mt-5">
+      <h1>Welcome to GIGA SPORTS SHOP</h1>
+
+      <p>Find high-quality Cricket, Football, Badminton and Gym Equipment.</p>
+
+      <button className="btn btn-primary">Shop Now</button>
+    </div>
+  );
+}
+
+export default Home;
