d9bc16cccba4b92e2375aa4ee735869bc16166a7
zain0333 <zain03337631969@gmail.com>
2026-08-05 01:27:23 -0700

    twenty commit

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -4,6 +4,11 @@ function Cart() {
   const { cart, removeFromCart, increaseQuantity, decreaseQuantity } =
     useCart();
 
+  const totalPrice = cart.reduce(
+    (total, item) => total + item.price * item.quantity,
+    0,
+  );
+
   return (
     <div className="container mt-4">
       <h1>Your Cart</h1>
@@ -11,51 +16,62 @@ function Cart() {
       {cart.length === 0 ? (
         <h4>Your cart is empty</h4>
       ) : (
-        <div className="row">
-          {cart.map((item) => (
-            <div className="col-md-4 mb-4" key={item.id}>
-              <div className="card shadow">
-                <img
-                  src={item.image}
-                  className="card-img-top"
-                  alt={item.name}
-                  style={{ height: "200px", objectFit: "contain" }}
-                />
+        <>
+          <div className="row">
+            {cart.map((item) => (
+              <div className="col-md-4 mb-4" key={item.id}>
+                <div className="card shadow">
+                  <img
+                    src={item.image}
+                    className="card-img-top"
+                    alt={item.name}
+                    style={{
+                      height: "200px",
+                      objectFit: "contain",
+                    }}
+                  />
 
-                <div className="card-body">
-                  <h5>{item.name}</h5>
+                  <div className="card-body">
+                    <h5>{item.name}</h5>
 
-                  <p>Price: Rs. {item.price}</p>
+                    <p>Price: Rs. {item.price}</p>
 
-                  <div className="d-flex align-items-center mb-3">
+                    <div className="d-flex align-items-center">
+                      <button
+                        className="btn btn-danger"
+                        onClick={() => decreaseQuantity(item.id)}
+                      >
+                        -
+                      </button>
+
+                      <span className="mx-3">{item.quantity}</span>
+
+                      <button
+                        className="btn btn-success"
+                        onClick={() => increaseQuantity(item.id)}
+                      >
+                        +
+                      </button>
+                    </div>
+
                     <button
-                      className="btn btn-danger"
-                      onClick={() => decreaseQuantity(item.id)}
+                      className="btn btn-outline-danger mt-3"
+                      onClick={() => removeFromCart(item.id)}
                     >
-                      -
-                    </button>
-
-                    <span className="mx-3">{item.quantity}</span>
-
-                    <button
-                      className="btn btn-success"
-                      onClick={() => increaseQuantity(item.id)}
-                    >
-                      +
+                      Remove
                     </button>
                   </div>
-
-                  <button
-                    className="btn btn-outline-danger"
-                    onClick={() => removeFromCart(item.id)}
-                  >
-                    Remove
-                  </button>
                 </div>
               </div>
-            </div>
-          ))}
-        </div>
+            ))}
+          </div>
+
+          <div className="card p-3 mt-3">
+            <h3>Total: Rs. {totalPrice}</h3>
+
+            <button className="btn btn-primary">Proceed to Checkout</button>
+          </div>
+        </>
       )}
     </div>
   );
