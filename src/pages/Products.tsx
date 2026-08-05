3b915b4ad45555d07b72e47dc766ad3774a8b9c2
zain0333 <zain03337631969@gmail.com>
2026-08-05 02:13:24 -0700

    twenty-eight commit

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,13 +1,30 @@
+import { useState } from "react";
 import ProductCard from "../components/ProductCard";
 import { products } from "../data/products";
 
 function Products() {
+  const [search, setSearch] = useState("");
+
+  const filteredProducts = products.filter((product) =>
+    product.name.toLowerCase().includes(search.toLowerCase()),
+  );
+
   return (
     <div className="container mt-4">
       <h1 className="mb-4">GIGA SPORTS PRODUCTS</h1>
 
+      {/* Search Bar */}
+
+      <input
+        type="text"
+        className="form-control mb-4"
+        placeholder="Search products..."
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+
       <div className="row">
-        {products.map((product) => (
+        {filteredProducts.map((product) => (
           <div className="col-md-4 mb-4" key={product.id}>
             <ProductCard
               id={product.id}
