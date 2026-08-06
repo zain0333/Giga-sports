0700e6ce763171a7ef19a5d6d79233f43bbd2529
zain0333 <zain03337631969@gmail.com>
2026-08-06 02:28:50 -0700

    new update

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,25 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-
 import Home from "./pages/Home";
 import Products from "./pages/Products";
-import ProductDetails from "./pages/ProductDetails";
-import Cart from "./pages/Cart";
-import Checkout from "./pages/Checkout";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
-
-import { CartProvider } from "./context/CartContext";
+import Cart from "./pages/Cart";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <CartProvider>
-      <BrowserRouter>
-        <Navbar />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
 
-        <Routes>
-          {/* Home */}
-          <Route path="/" element={<Home />} />
-
-          {/* Products */}
-          <Route path="/products" element={<Products />} />
-
-          {/* Product Details */}
-          <Route path="/product/:id" element={<ProductDetails />} />
-
-          {/* Cart */}
-          <Route path="/cart" element={<Cart />} />
-
-          {/* Checkout */}
-          <Route path="/checkout" element={<Checkout />} />
-
-          {/* About */}
-          <Route path="/about" element={<About />} />
-
-          {/* Contact */}
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-
-        <Footer />
-      </BrowserRouter>
-    </CartProvider>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
