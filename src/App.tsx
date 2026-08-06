7c1b347a35d4d7aa713b7cb826ca7db01dfb998e
zain0333 <zain03337631969@gmail.com>
2026-08-06 02:40:15 -0700

    new update

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,33 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import { CartProvider } from "./context/CartContext";
+
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
 import Home from "./pages/Home";
 import Products from "./pages/Products";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Cart from "./pages/Cart";
-import Footer from "./components/Footer";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/cart" element={<Cart />} />
-      </Routes>
+    <CartProvider>
+      <BrowserRouter>
+        <Navbar />
 
-      <Footer />
-    </BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+
+        <Footer />
+      </BrowserRouter>
+    </CartProvider>
   );
 }
 
