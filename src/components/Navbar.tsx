795dbd3ba654d54ad2e0996555fae8bbda5e6eef
zain0333 <zain03337631969@gmail.com>
2026-08-10 23:29:53 -0700

    redesigning

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,61 +1,28 @@
 import { Link } from "react-router-dom";
-import { FaShoppingCart } from "react-icons/fa";
+import { FaShoppingCart, FaBolt } from "react-icons/fa";
 import { useCart } from "../context/CartContext";
 
 function Navbar() {
   const { cart } = useCart();
 
-  const cartCount = cart.reduce((total, item) => total + item.quantity, 0);
-
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-      <div className="container">
-        <Link className="navbar-brand fw-bold" to="/">
+    <nav className="main-navbar">
+      <div className="navbar-container">
+        <Link to="/" className="brand">
+          <FaBolt className="brand-icon" />
           GIGA SPORTS SHOP
         </Link>
 
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#menu"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
+        <div className="nav-links">
+          <Link to="/">Home</Link>
+          <Link to="/products">Products</Link>
+          <Link to="/about">About</Link>
+          <Link to="/contact">Contact</Link>
 
-        <div className="collapse navbar-collapse" id="menu">
-          <ul className="navbar-nav ms-auto">
-            <li className="nav-item">
-              <Link className="nav-link" to="/">
-                Home
-              </Link>
-            </li>
-
-            <li className="nav-item">
-              <Link className="nav-link" to="/products">
-                Products
-              </Link>
-            </li>
-
-            <li className="nav-item">
-              <Link className="nav-link" to="/about">
-                About
-              </Link>
-            </li>
-
-            <li className="nav-item">
-              <Link className="nav-link" to="/contact">
-                Contact
-              </Link>
-            </li>
-
-            <li className="nav-item">
-              <Link className="nav-link" to="/cart">
-                <FaShoppingCart />
-                Cart ({cartCount})
-              </Link>
-            </li>
-          </ul>
+          <Link to="/cart" className="cart-link">
+            <FaShoppingCart />
+            Cart ({cart.length})
+          </Link>
         </div>
       </div>
     </nav>
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-import App from "./App.tsx";
-import "./App.css";
+import App from "./App";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+  </React.StrictMode>,
 );
