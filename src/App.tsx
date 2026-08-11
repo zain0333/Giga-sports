71deec240676647b84916efb2ead5aeb3fc8759e
zain0333 <zain03337631969@gmail.com>
2026-08-11 03:57:39 -0700

    last commit

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,6 @@ import Products from "./pages/Products";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Cart from "./pages/Cart";
-import Checkout from "./pages/Checkout";
-import OrderTracking from "./pages/OrderTracking";
 
 function App() {
   return (
@@ -17,15 +15,20 @@ function App() {
       <Navbar />
 
       <Routes>
+        {/* Home */}
         <Route path="/" element={<Home />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/checkout" element={<Checkout />} />
 
-        {/* Order Tracking */}
-        <Route path="/order-tracking" element={<OrderTracking />} />
+        {/* Products */}
+        <Route path="/products" element={<Products />} />
+
+        {/* About */}
+        <Route path="/about" element={<About />} />
+
+        {/* Contact */}
+        <Route path="/contact" element={<Contact />} />
+
+        {/* Cart */}
+        <Route path="/cart" element={<Cart />} />
       </Routes>
 
       <Footer />
--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -3,23 +3,27 @@ import { products } from "../data/products";
 
 function Products() {
   return (
-    <div className="container py-4">
-      <h1 className="text-center mb-4">🏆 GIGA SPORTS SHOP</h1>
+    <main className="products-page">
+      <div className="container my-5">
+        {/* Page Heading */}
+        <div className="text-center mb-5">
+          <h1 className="fw-bold">Our Sports Products</h1>
 
-      <div className="row">
-        {products.map((product) => (
-          <div className="col-md-3 mb-4" key={product.id}>
-            <ProductCard
-              id={product.id}
-              name={product.name}
-              category={product.category}
-              price={product.price}
-              image={product.image}
-            />
-          </div>
-        ))}
+          <p className="text-muted">
+            Find the best sports equipment at GIGA SPORTS SHOP
+          </p>
+        </div>
+
+        {/* Products */}
+        <div className="row">
+          {products.map((product) => (
+            <div className="col-12 col-sm-6 col-lg-3 mb-4" key={product.id}>
+              <ProductCard product={product} />
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
+    </main>
   );
 }
 
